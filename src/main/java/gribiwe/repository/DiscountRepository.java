6dcb1a90628d937e631327e079649653bb810993
Gribiwe <grib.morpech@gmail.com>
2018-10-23 20:01:15 +0300

    fixed from recipient to discount as generic

--- src/main/java/gribiwe/repository/DiscountRepository.java
+++ src/main/java/gribiwe/repository/DiscountRepository.java
@@ -1,9 +1,9 @@
 package gribiwe.repository;
 
-import gribiwe.entity.RecipientEntity;
+import gribiwe.entity.DiscountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface DiscountRepository extends JpaRepository<RecipientEntity, UUID> {
+public interface DiscountRepository extends JpaRepository<DiscountEntity, UUID> {
 }
