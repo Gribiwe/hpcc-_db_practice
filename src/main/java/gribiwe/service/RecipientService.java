932b8ebc9ed14107d5984b4f75c1295fc652124c
Gribiwe <grib.morpech@gmail.com>
2018-10-23 17:42:29 +0300

    registration and verification

--- src/main/java/gribiwe/service/RecipientService.java
+++ src/main/java/gribiwe/service/RecipientService.java
@@ -1,8 +1,12 @@
 package gribiwe.service;
 
+import gribiwe.entity.RecipientEntity;
+import gribiwe.exception.RecipientExistsException;
 import gribiwe.repository.RecipientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class RecipientService {
@@ -14,4 +18,30 @@ public class RecipientService {
       this.recipientRepository = recipientRepository;
    }
 
+   public void registerNew(String name, String address, String phone) throws RecipientExistsException {
+      boolean phoneExists = isExistsPhone(address);
+      boolean nameExists = isExistsName(name);
+      if (!(phoneExists || nameExists)) {
+         RecipientEntity recipientEntity = new RecipientEntity();
+         recipientEntity.setId(UUID.randomUUID());
+         recipientEntity.setAddress(address);
+         recipientEntity.setName(name);
+         recipientEntity.setPhone(phone);
+         recipientRepository.save(recipientEntity);
+      } else {
+         throw new RecipientExistsException("Recipient with this name and phone is already exists", phoneExists, nameExists);
+      }
+   }
+
+   public RecipientEntity getRecipient(UUID uuid) {
+      return recipientRepository.findOne(uuid);
+   }
+
+   public boolean isExistsPhone(String address) {
+      return recipientRepository.existsRecipientEntitiesByPhone(address);
+   }
+
+   public boolean isExistsName(String name) {
+      return recipientRepository.existsRecipientEntitiesByName(name);
+   }
 }
