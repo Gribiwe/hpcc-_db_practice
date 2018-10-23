d329445401920e31988df3e457c0b511a8f3cf92
Gribiwe <grib.morpech@gmail.com>
2018-10-23 17:42:55 +0300

    methods for checking existing

--- src/main/java/gribiwe/repository/RecipientRepository.java
+++ src/main/java/gribiwe/repository/RecipientRepository.java
@@ -6,4 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface RecipientRepository extends JpaRepository<RecipientEntity, UUID> {
+
+   boolean existsRecipientEntitiesByPhone(String phone);
+
+   boolean existsRecipientEntitiesByName(String name);
 }
