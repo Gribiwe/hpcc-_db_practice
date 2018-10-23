0005d03978e50e79605e6bfc38b7a0ea553c4637
Gribiwe <grib.morpech@gmail.com>
2018-10-23 20:02:31 +0300

    adding new discount

--- src/main/java/gribiwe/service/DiscountService.java
+++ src/main/java/gribiwe/service/DiscountService.java
@@ -1,8 +1,11 @@
 package gribiwe.service;
 
+import gribiwe.entity.DiscountEntity;
 import gribiwe.repository.DiscountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class DiscountService {
@@ -12,5 +15,15 @@ public class DiscountService {
    @Autowired
    public DiscountService(DiscountRepository discountRepository) {
       this.discountRepository = discountRepository;
+   }
+
+   public DiscountEntity addNew(Double upperThreshold, Double lowerThreshold, Double amount) {
+      DiscountEntity discountEntity = new DiscountEntity();
+      discountEntity.setId(UUID.randomUUID());
+      discountEntity.setAmount(amount);
+      discountEntity.setUpperThreshold(upperThreshold);
+      discountEntity.setLowerThreshold(lowerThreshold);
+      discountRepository.save(discountEntity);
+      return discountEntity;
    }
 }
